ed905f7759b892054e33e71fbd763e09743e5bb0
Alea Chlodnik <achlodnik@gmail.com>
2024-04-08 15:28:18 +0200

    [ADD] usage message

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,11 @@ module Main (main) where
 
 import Lib
 
+usage :: IO ()
+usage = putStrLn "USAGE: ./mypandoc -i ifile -f oformat [-o ofile] [-e iformat]\
+                \\n\t ifile\tpath to the file to convert\n\toformat\toutput \
+                \format (xml, json, markdown)\n\tofile\tpath to the output \
+                \file\n\tiformat\tinput format (xml, json, markdown)"
+
 main :: IO ()
 main = print $ parseChar 'a' "aaaa"
-
