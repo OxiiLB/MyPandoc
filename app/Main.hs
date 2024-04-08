379250f03dc19766b03109279855ff4887e77f7a
Alea Chlodnik <achlodnik@gmail.com>
2024-04-08 16:03:09 +0200

    [ADD] src files

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,15 @@
 
 module Main (main) where
 
+import ErrorHandling
+import Parser
 import Lib
 
 usage :: IO ()
 usage = putStrLn "USAGE: ./mypandoc -i ifile -f oformat [-o ofile] [-e iformat]\
-                \\n\t ifile\tpath to the file to convert\n\toformat\toutput \
+                \\n\n\tifile\tpath to the file to convert\n\toformat\toutput \
                 \format (xml, json, markdown)\n\tofile\tpath to the output \
                 \file\n\tiformat\tinput format (xml, json, markdown)"
 
 main :: IO ()
-main = print $ parseChar 'a' "aaaa"
+main = usage
