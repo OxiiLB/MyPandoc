a31152e04e56ed2b1d1e7de24512ea8244c9bb3b
Alea Chlodnik <achlodnik@gmail.com>
2024-04-16 17:38:41 +0200

    [UPD] ErrorHandling - checks flags, repeat flags, and format

--- src/ErrorHandling.hs
+++ src/ErrorHandling.hs
@@ -9,25 +9,40 @@ module ErrorHandling
     ( checkArgs,
     ) where
 
--- check flag values
--- (args !! 1 /= "xml") && (args !! 1 /= "json") && (args !! 1 /= "markdown") = False
+import Data.List (nub)
+
+checkFormatArgs :: [String] -> Bool
+checkFormatArgs [] = True
+checkFormatArgs ("-f":val:rest) 
+    | val `notElem` ["xml", "json", "markdown"] = False
+    | otherwise = checkFormatArgs rest
+checkFormatArgs ("-e":val:rest) 
+    | val `notElem` ["xml", "json", "markdown"] = False
+    | otherwise = checkFormatArgs rest
+checkFormatArgs (_:rest) = checkFormatArgs rest
+
+checkFlagRepeats :: [String] -> Bool
+checkFlagRepeats [] = False
+checkFlagRepeats args | length args /= length (nub args) = True
+                      | otherwise = False
 
 checkFlags :: [String] -> Bool
 checkFlags [] = False
-checkFlags args | not ("-i" `elem` args) || not ("-f" `elem` args) = False
-    | (args !! 0 /= "-i" && args !! 0 /= "-f" && args !! 0 /= "-o"
-        && args !! 0 /= "-e") = False
-    | (args !! 2 /= "-i" && args !! 2 /= "-f" && args !! 2 /= "-o"
-        && args !! 2 /= "-e") = False
-    | (length args >= 5 && (args !! 4 /= "-i" && args !! 4 /= "-f"
-        && args !! 4 /= "-o" && args !! 4 /= "-e")) = False
-    | (length args >= 7 && (args !! 6 /= "-i" && args !! 6 /= "-f"
-        && args !! 6 /= "-o" && args !! 6 /= "-e")) = False
+checkFlags args | notElem "-i" args || notElem "-f" args = False
+    | head args `notElem` ["-i", "-f", "-o", "-e"] = False
+    | length args >= 3 &&
+        (args !! 2) `notElem` ["-i", "-f", "-o", "-e"] = False
+    | length args >= 5 &&
+        (args !! 4) `notElem` ["-i", "-f", "-o", "-e"] = False
+    | length args == 7 &&
+        (args !! 6) `notElem` ["-i", "-f", "-o", "-e"] = False
     | otherwise = True
 
 checkArgs :: [String] -> Bool
 checkArgs [] = False
 checkArgs args
-    | length args < 4 || length args > 7 = False
-    | checkFlags args == False = False
+    | length args `notElem` [4, 6, 8] = False
+    | not (checkFlags args) = False
+    | checkFlagRepeats args = False
+    | not (checkFormatArgs args) = False
     | otherwise = True
