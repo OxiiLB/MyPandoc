993c3535235f3a70b44621df4cde0a61dcd57f6f
arthurgauffre <arth.gauffre@gmail.com>
2024-04-22 15:47:28 +0200

    [UPD] Refactor parseJsonValue function in Lib.hs, it doesnt work for now

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,169 +1,79 @@
-{--
--- EPITECH PROJECT, 2024
--- Lib
--- File description:
--- Lib
---}
-{-# LANGUAGE InstanceSigs #-}
-
 module Lib
-    ( parseChar
-    , runParser
-    , parseAnyChar
-    , parseOr
-    , parseAnd
-    , parseAndWith
-    , parseMany
-    , parseSome
-    , parseUInt
-    , parseInt
-    , parseTuple
+    (
     ) where
-import Control.Applicative()
-import Control.Applicative (Alternative(..))
-import Control.Monad (ap)
 
+import Control.Applicative ((<|>), some, many)
+import Text.ParserCombinators.Parsec.Char (digit, char, string, oneOf, noneOf)
+import Data.Char (chr)
+import Numeric (readHex)
 
+-- Define your Parser type and itds associated functions here
 data Parser a = Parser {
     runParser :: String -> Maybe (a , String )
 }
 
-instance Monad Parser where
-    return = pure
-    p >>= f = Parser $ \s -> case runParser p s of
-        Nothing -> Nothing
-        Just (a, rest) -> runParser (f a) rest
+-- Define your JsonValue data type
+data JsonValue
+    = JsonNull
+    | JsonBool Bool
+    | JsonNumber Double
+    | JsonString String
+    | JsonArray [JsonValue]
+    | JsonObject [(String, JsonValue)]
+    deriving (Show)
 
--- Define `ap` for Parser to avoid orphan instances
-instance Applicative Parser where
-    pure x = Parser $ \s -> Just (x, s)
-    (<*>) = ap
+-- Define your parsers for JsonValue
+parseJsonValue :: Parser JsonValue
+parseJsonValue = parseNull <|> parseBool <|> parseNumber <|> parseString
 
+parseNull :: Parser JsonValue
+parseNull = JsonNull <$ string "null"
 
+parseBool :: Parser JsonValue
+parseBool = (JsonBool True <$ string "true") <|> (JsonBool False <$ string "false")
 
-instance Alternative Parser where
-    empty = Parser $ const Nothing
-    p1 <|> p2 = Parser $ \s -> case runParser p1 s of
-        Nothing -> runParser p2 s
-        Just (a, rest) -> Just (a, rest)
+parseNumber :: Parser JsonValue
+parseNumber = JsonNumber <$> parseFloat
 
+parseString :: Parser JsonValue
+parseString = JsonString <$> parseQuotedString
 
--- instance Applicative Parser where
---     pure x = Parser $ \s -> Just (x, s)
---     pf <*> px = Parser $ \s -> case runParser pf s of
---         Nothing -> Nothing
---         Just (f, rest) -> case runParser px rest of
---             Nothing -> Nothing
---             Just (x, rest') -> Just (f x, rest')
+-- parseFloat parser
+parseFloat :: Parser Double
+parseFloat = read <$> (plus <|> minus <|> num <|> decimal <|> exponent)
+  where
+    plus = char '+' *> num
+    minus = (:) <$> char '-' <*> num
+    num = some digit
+    decimal = (++) <$> num <*> ((:) <$> char '.' <*> num)
+    exponent = do
+        e <- oneOf "eE"
+        sign <- optional (char '+' <|> char '-')
+        exp <- num
+        let multiplier = case sign of
+                Just '+' -> 1
+                _        -> -1
+        return $ "e" ++ [e] ++ show (multiplier * exp)
 
-
-instance Functor Parser where
-    fmap fct parser = Parser $ \s -> case runParser parser s of
-        Nothing -> Nothing
-        Just (a, rest) -> Just (fct a, rest)
-
-parseChar :: Char -> Parser Char
-parseChar c = Parser $ \s -> case s of
-    [] -> Nothing
-    (x:xs) -> if x == c
-              then Just (x, xs)
-              else Nothing
-
-parseAnyChar :: String -> Parser Char
-parseAnyChar cs = Parser $ \s -> case s of
-    [] -> Nothing
-    (x:xs) -> if x `elem` cs
-              then Just (x, xs)
-              else Nothing
-
-parseOr :: Parser a -> Parser a -> Parser a
-parseOr f1 f2 = Parser $ \s -> case runParser f1 s of
-    Nothing -> runParser f2 s
-    Just (a, rest) -> Just (a, rest)
-
-parseAnd :: Parser a -> Parser b -> Parser (a, b)
-parseAnd f1 f2 = Parser $ \s -> case runParser f1 s of
-    Nothing -> Nothing
-    Just (a, rest) -> case runParser f2 rest of
-        Nothing -> Nothing
-        Just (b, c) -> Just ((a, b), c)
-
--- STEP 2.3
-parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-parseAndWith f f1 f2 = f <$> f1 <*> f2
-
--- parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
--- parseAndWith f f1 f2 = Parser $ \s -> case runParser f1 s of
---     Nothing -> Nothing
---     Just (a, rest) -> case runParser f2 rest of
---         Nothing -> Nothing
---         Just (b, c) -> Just (f a b, c)
-
--- STEP 2.4
-parseMany :: Parser a -> Parser [a]
-parseMany f = someOrNone
-    where
-        someOrNone = (:) <$> f <*> parseMany f <|> pure []
-
--- parseMany :: Parser a -> Parser [a]
--- parseMany f = Parser $ \s -> case runParser f s of
---     Nothing -> Just ([], s)
---     Just (a, rest) -> case runParser (parseMany f) rest of
---         Nothing -> Nothing
---         Just (as, c) -> Just (a:as, c)
-
--- STEP 2.3
-parseSome :: Parser a -> Parser [a]
-parseSome f = (:) <$> f <*> parseMany f
-
--- parseSome :: Parser a -> Parser [a]
--- parseSome f = Parser $ \s -> case runParser f s of
---     Nothing -> Nothing
---     Just (a, rest) -> case runParser (parseMany f) rest of
---         Nothing -> Just ([a], rest)
---         Just (as, c) -> Just (a:as, c)
-
--- STEP 2.2 and 2.3 and 2.4 <$>
-parseInt :: Parser Int
-parseInt = read <$> parseSome (parseAnyChar ['0'..'9'])
-
--- STEP 2.2 FMAP
--- parseInt :: Parser Int
--- parseInt = fmap read (parseSome (parseAnyChar ['0'..'9']))
-
--- parseInt :: Parser Int
--- parseInt = Parser $ \s -> case runParser (parseSome (parseAnyChar ['0'..'9'])) s of
---     Nothing -> Nothing
---     Just (as, rest) -> Just (read as, rest)
-
-parseUInt :: Parser Int
-parseUInt = Parser $ \s -> case runParser (parseSome (parseAnyChar ['0'..'9'])) s of
-    Nothing -> Nothing
-    Just (as, rest) -> Just (read as, rest)
-
--- STEP 2.5
-parseTuple :: Parser (Int, Int, Int)
-parseTuple = do
-    parseChar '('
-    a <- parseInt
-    parseChar ','
-    b <- parseInt
-    parseChar ','
-    c <- parseInt
-    parseChar ')'
-    return (a, b, c)
-
-
--- STEP 2.3
--- parseTuple :: Parser a -> Parser (a, a)
--- parseTuple f = (,) <$> f <* parseChar ',' <*> f
-
--- parseTuple :: Parser a -> Parser (a, a)
--- parseTuple f s = case parseChar '(' s of
---                     Nothing -> Nothing
---                     Just (_,y) -> case parseAndWith (\a (_, b) -> (a, b)) f
---                         (parseAnd (parseChar ',') f) y of
---                         Nothing -> Nothing
---                         Just (a, res) -> case parseChar ')' res of
---                             Nothing -> Nothing
---                             Just (_, rest) -> Just (a, rest)
+-- parseQuotedString parser
+parseQuotedString :: Parser String
+parseQuotedString = char '"' *> many (escapedChar <|> noneOf "\"") <* char '"'
+  where
+    escapedChar = char '\\' *> choice
+        [ '"'  <$ char '"'
+        , '\\' <$ char '\\'
+        , '/'  <$ char '/'
+        , '\b' <$ char 'b'
+        , '\f' <$ char 'f'
+        , '\n' <$ char 'n'
+        , '\r' <$ char 'r'
+        , '\t' <$ char 't'
+        , '\v' <$ char 'v'
+        , '&' <$ string "&amp;" -- XML escape for "&"
+        , '\t' <$ string "&gt;"  -- XML escape for ">"
+        , '\t' <$ string "&lt;"  -- XML escape for "<"
+        , '\t' <$ string "&quot;"-- XML escape for """
+        , '\t' <$ string "&apos;"-- XML escape for "'"
+        , hexChar
+        ]
+    hexChar = char 'u' *> (chr . fromInteger . fst . head . readHex <$> count 4 digit)
