e5b07827cc462b453def36b386ea60d088649f34
arthurgauffre <arth.gauffre@gmail.com>
2024-04-22 13:51:02 +0200

    [ADD] step2.1, change function to handle new parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,82 +7,93 @@
 
 module Lib
     ( parseChar
+    , runParser
     , parseAnyChar
-    , parseOr
-    , parseAnd
-    , parseAndWith
-    , parseMany
-    , parseSome
-    , parseUInt
-    , parseInt
-    , parseTuple
+    -- , parseOr
+    -- , parseAnd
+    -- , parseAndWith
+    -- , parseMany
+    -- , parseSome
+    -- , parseUInt
+    -- , parseInt
+    -- , parseTuple
     ) where
 
-type Parser a = String -> Maybe (a , String )
+data Parser a = Parser {
+    runParser :: String -> Maybe (a , String )
+}
 
 parseChar :: Char -> Parser Char
-parseChar _ [] = Nothing
-parseChar c (x:xs) = if x == c
-                   then Just (x, xs)
-                   else Nothing
+parseChar c = Parser $ \s -> case s of
+    [] -> Nothing
+    (x:xs) -> if x == c
+              then Just (x, xs)
+              else Nothing
 
 parseAnyChar :: String -> Parser Char
-parseAnyChar _ [] = Nothing
-parseAnyChar [] _ = Nothing
-parseAnyChar (c:cs) (x:xs) = if x == c
-                             then Just (x, xs)
-                             else parseAnyChar cs (x:xs)
+parseAnyChar cs = Parser $ \s -> case s of
+    [] -> Nothing
+    (x:xs) -> if x `elem` cs
+              then Just (x, xs)
+              else Nothing
 
-parseOr :: Parser a -> Parser a -> Parser a
-parseOr f1 f2 s = case f1 s of
-                    Nothing -> f2 s
-                    Just (a, rest) -> Just (a, rest)
+-- parseAnyChar :: String -> Parser Char
+-- parseAnyChar _ [] = Nothing
+-- parseAnyChar [] _ = Nothing
+-- parseAnyChar (c:cs) (x:xs) = if x == c
+--                              then Just (x, xs)
+--                              else parseAnyChar cs (x:xs)
 
-parseAnd :: Parser a -> Parser b -> Parser (a, b)
-parseAnd f1 f2 s = case f1 s of
-                    Nothing -> Nothing
-                    Just (a, rest) -> case f2 rest of
-                        Nothing -> Nothing
-                        Just (b, c) -> Just ((a, b), c)
+-- parseOr :: Parser a -> Parser a -> Parser a
+-- parseOr f1 f2 s = case f1 s of
+--                     Nothing -> f2 s
+--                     Just (a, rest) -> Just (a, rest)
 
-parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-parseAndWith f f1 f2 s = case f1 s of
-                    Nothing -> Nothing
-                    Just (a, rest) -> case f2 rest of
-                        Nothing -> Nothing
-                        Just (b, c) -> Just (f a b, c)
+-- parseAnd :: Parser a -> Parser b -> Parser (a, b)
+-- parseAnd f1 f2 s = case f1 s of
+--                     Nothing -> Nothing
+--                     Just (a, rest) -> case f2 rest of
+--                         Nothing -> Nothing
+--                         Just (b, c) -> Just ((a, b), c)
 
-parseMany :: Parser a -> Parser [a]
-parseMany f s = case f s of
-                    Nothing -> Just ([], s)
-                    Just (a, rest) -> case parseMany f rest of
-                        Nothing -> Nothing
-                        Just (as, c) -> Just (a:as, c)
+-- parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+-- parseAndWith f f1 f2 s = case f1 s of
+--                     Nothing -> Nothing
+--                     Just (a, rest) -> case f2 rest of
+--                         Nothing -> Nothing
+--                         Just (b, c) -> Just (f a b, c)
 
-parseSome :: Parser a -> Parser [a]
-parseSome f s = case f s of
-                    Nothing -> Nothing
-                    Just (a, rest) -> case parseMany f rest of
-                        Nothing -> Just ([a], rest)
-                        Just (as, c) -> Just (a:as, c)
+-- parseMany :: Parser a -> Parser [a]
+-- parseMany f s = case f s of
+--                     Nothing -> Just ([], s)
+--                     Just (a, rest) -> case parseMany f rest of
+--                         Nothing -> Nothing
+--                         Just (as, c) -> Just (a:as, c)
 
-parseUInt :: Parser Int
-parseUInt s = case parseSome (parseAnyChar ['0'..'9']) s of
-                Nothing -> Nothing
-                Just (as, rest) -> Just (read as, rest)
+-- parseSome :: Parser a -> Parser [a]
+-- parseSome f s = case f s of
+--                     Nothing -> Nothing
+--                     Just (a, rest) -> case parseMany f rest of
+--                         Nothing -> Just ([a], rest)
+--                         Just (as, c) -> Just (a:as, c)
 
-parseInt :: Parser Int
-parseInt s = case parseOr (parseAndWith (\_ b -> b) (parseChar '-') parseUInt)
-                parseUInt s of
-                Nothing -> Nothing
-                Just (a, rest) -> Just (a, rest)
+-- parseUInt :: Parser Int
+-- parseUInt s = case parseSome (parseAnyChar ['0'..'9']) s of
+--                 Nothing -> Nothing
+--                 Just (as, rest) -> Just (read as, rest)
 
-parseTuple :: Parser a -> Parser (a, a)
-parseTuple f s = case parseChar '(' s of
-                    Nothing -> Nothing
-                    Just (_,y) -> case parseAndWith (\a (_, b) -> (a, b)) f
-                        (parseAnd (parseChar ',') f) y of
-                        Nothing -> Nothing
-                        Just (a, res) -> case parseChar ')' res of
-                            Nothing -> Nothing
-                            Just (_, rest) -> Just (a, rest)
+-- parseInt :: Parser Int
+-- parseInt s = case parseOr (parseAndWith (\_ b -> b) (parseChar '-') parseUInt)
+--                 parseUInt s of
+--                 Nothing -> Nothing
+--                 Just (a, rest) -> Just (a, rest)
+
+-- parseTuple :: Parser a -> Parser (a, a)
+-- parseTuple f s = case parseChar '(' s of
+--                     Nothing -> Nothing
+--                     Just (_,y) -> case parseAndWith (\a (_, b) -> (a, b)) f
+--                         (parseAnd (parseChar ',') f) y of
+--                         Nothing -> Nothing
+--                         Just (a, res) -> case parseChar ')' res of
+--                             Nothing -> Nothing
+--                             Just (_, rest) -> Just (a, rest)
