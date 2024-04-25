bfd18650c15e7f0064a4a7a4a354630d22bb1aa3
OxiiLB <romain.giraud@epitech.eu>
2024-04-25 15:58:10 +0200

    [UPDATE] outputs

---
+++ src/OutputMarkdown.hs
@@ -0,0 +1,24 @@
+{--
+-- EPITECH PROJECT, 2024
+-- MyPandoc
+-- File description:
+-- OutputMarkdown
+--}
+
+module OutputMarkdown
+    ( writeMarkdownFile
+    ) where
+
+import Parser ( ParserValue(..) )
+import Data.List ( intercalate )
+
+toMarkdown :: ParserValue -> Int -> String
+toMarkdown (ParserString s) _ = s
+toMarkdown (ParserArray arr) _ = "-" ++ intercalate "\n-" (map (`toMarkdown` 0) arr)
+
+toMarkdown (ParserObject obj) level = intercalate "\n" $ map (\(k, v) ->
+    replicate (level * 4) ' ' ++ "- " ++ k ++ ":\n" ++ toMarkdown v (level + 1)) obj
+
+writeMarkdownFile :: Maybe FilePath -> ParserValue -> IO ()
+writeMarkdownFile Nothing _ = putStrLn "No file path provided."
+writeMarkdownFile (Just path) value = writeFile path (toMarkdown value 0)
